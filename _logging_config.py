fe7ccc543b0dbe4aabd56575aa7e923e8a2f20ce
Kang Chen <kangchen@nypl.org>
2024-07-03 18:46:23 -0400

    update logging

--- _logging_config.py
+++ _logging_config.py
@@ -1,10 +1,12 @@
 import logging
 
+LEVEL = logging.DEBUG
 
-def _get_logger(level: int = logging.INFO):
+
+def _get_logger(level: int = logging.INFO) -> logging.Logger:
     format = "%(levelname).8s %(name).12s %(asctime)s %(message)s"
     datefmt = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(level=level, format=format, datefmt=datefmt)
+    logging.basicConfig(level=LEVEL, format=format, datefmt=datefmt)
 
     logger = logging.getLogger(__name__)
     return logger
